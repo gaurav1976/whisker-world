efed347fbb66aef7a639468655ce2c3d0d82260c
gaurav1976 <gaurav.webyug@gmail.com>
2025-05-27 16:58:52 +0530

    Update vite.config.js

--- react_navbar/vite.config.js
+++ react_navbar/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/whisker-world",
+  base: process.env.VITE_BASE_PATH || "/gaurav1976",
 })
