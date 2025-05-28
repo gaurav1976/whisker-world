cc59c981412a04765602fc57a3d14046689f19ba
gaurav1976 <gaurav.webyug@gmail.com>
2025-05-28 20:24:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ if (!fs.existsSync(uploadDir)) {
 
 // ✅ Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((error) => console.log("❌ MongoDB Connection Error:", error));
 
