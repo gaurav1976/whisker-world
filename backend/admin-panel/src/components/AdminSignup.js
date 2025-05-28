fcf0e2680bd44df16b50ece3df233bdb4a4db593
gaurav1976 <gaurav.webyug@gmail.com>
2025-05-28 18:53:04 +0530

    Update AdminSignup.js

--- backend/admin-panel/src/components/AdminSignup.js
+++ backend/admin-panel/src/components/AdminSignup.js
@@ -97,7 +97,7 @@ function AdminSignup() {
       <h2>Admin Signup</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label>Name</label>
+          <label>Nameeee</label>
           <input 
             type="text" 
             name="name" 
