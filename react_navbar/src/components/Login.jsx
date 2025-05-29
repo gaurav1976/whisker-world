e3d9657b8dfb6ac79525bc7a8f341a6858be8e8e
gaurav1976 <gaurav.webyug@gmail.com>
2025-05-29 12:13:46 +0530

    Update Login.jsx

--- react_navbar/src/components/Login.jsx
+++ react_navbar/src/components/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
         }
 
         try {
-            const response = await fetch('${process.env.REACT_APP_API_BASE_URL}/login', {
+            const response = await fetch('https://whisker-world-qlpf.onrender.com/login', {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
