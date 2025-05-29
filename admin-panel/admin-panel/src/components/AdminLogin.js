4f5a2343de824b7050a2870e97fa06e8eed1099b
gaurav1976 <gaurav.webyug@gmail.com>
2025-05-29 18:58:29 +0530

    Update AdminLogin.js

--- admin-panel/admin-panel/src/components/AdminLogin.js
+++ admin-panel/admin-panel/src/components/AdminLogin.js
@@ -50,8 +50,8 @@ function AdminLogin() {
 
     setIsLoading(true);
     try {
-      const res = await axios.post(
-        "http://localhost:5000/admin/login",
+      const API_BASE = import.meta.env.VITE_API_BASE_URL;
+const response = await fetch(`${API_BASE}/login`, 
         formData,
         { headers: { "Content-Type": "application/json" } }
       );
