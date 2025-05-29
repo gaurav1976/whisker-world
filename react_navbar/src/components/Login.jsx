b7950ce9fa95d332eb3d2ef07e8ca20532e36e8c
gaurav1976 <gaurav.webyug@gmail.com>
2025-05-29 12:27:24 +0530

    Update Login.jsx

--- react_navbar/src/components/Login.jsx
+++ react_navbar/src/components/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
         }
 
         try {
-            const response = await fetch('${API_BASE}/login', {
+            const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
