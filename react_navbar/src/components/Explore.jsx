57f8363734d14a662783033003c3b7695184f1b7
gaurav1976 <gaurav.webyug@gmail.com>
2025-06-03 11:19:31 +0530

    Update Explore.jsx

--- react_navbar/src/components/Explore.jsx
+++ react_navbar/src/components/Explore.jsx
@@ -21,6 +21,7 @@ const Explore = () => {
   // Fetch Blogs from API and shuffle
  useEffect(() => {
   const API_BASE = import.meta.env.VITE_API_BASE_URL;
+  console.log("hii",API_BASE);
   axios.get(`${API_BASE}/explore`)
     .then((response) => {
       const shuffled = shuffleArray(response.data);
