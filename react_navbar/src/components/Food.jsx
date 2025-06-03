8ff102df889bf7a2cefb06e645ff1a36156c74e2
gaurav1976 <gaurav.webyug@gmail.com>
2025-06-03 17:54:30 +0530

    Update Food.jsx

--- react_navbar/src/components/Food.jsx
+++ react_navbar/src/components/Food.jsx
@@ -28,8 +28,7 @@ const shuffleArray = (array) => {
     // Fetch food items from backend
     const API_BASE = import.meta.env.VITE_API_BASE_URL;
    useEffect(() => {
-  
-
+console.log("heyy you",API_BASE);
   axios.get(`${API_BASE}/foods`) // make sure it matches backend route
     .then((res) => {
       const shuffledData = shuffleArray(res.data);
