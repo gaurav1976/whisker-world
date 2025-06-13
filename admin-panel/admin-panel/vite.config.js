348a517036355303ea1aaca74409fe63abf6c867
gaurav1976 <gaurav.webyug@gmail.com>
2025-06-13 18:38:48 +0530

    bdf

--- admin-panel/admin-panel/vite.config.js
+++ admin-panel/admin-panel/vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  base: '/', // <--- this is correct for Vercel
-}); 
+  base: "./", // important for relative paths in Vercel
+});
